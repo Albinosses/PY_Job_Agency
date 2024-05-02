61a21ff39b68f855ecddc1d79aa0ea82febf2834
Vasyil Syman <vsyman9@gmail.com>
2024-05-02 17:13:10 +0300

    feat: added animation when items list is empty

--- frontend/src/components/interviewPage/interviewScrollable/InterviewScrollable.js
+++ frontend/src/components/interviewPage/interviewScrollable/InterviewScrollable.js
@@ -3,34 +3,43 @@ import Interview from "../interview/Interview";
 import styles from "./InterviewScrollable.module.css";
 import {InterviewContext} from "../../../contexts/InterviewContext";
 import {Link} from "react-router-dom";
+import {CircularProgress} from "@mui/material";
 
 function InterviewScrollable({vacancyId}) {
     const {interviews} = useContext(InterviewContext)
 
     return (
-        <div className={styles.container}>
-            <div className={styles.scrollableContainer}>
-                {vacancyId ? (
-                    interviews
-                        .filter(interview => interview.vacancyId === vacancyId)
-                        .map(interview => (
+        <>
+            {interviews.length === 0 &&
+                <div className={styles.circularProgressContainer}>
+                    <CircularProgress size={80} thickness={4}/>
+                </div>
+            }
+            <div className={styles.container}>
+                <div className={styles.scrollableContainer}>
+                    {vacancyId ? (
+                        interviews
+                            .filter(interview => interview.vacancyId === vacancyId)
+                            .map(interview => (
+                                <Link to={`/interview/${interview.id}`}>
+                                    <Interview
+                                        key={interview.id}
+                                        vacancy={interview}
+                                    />
+                                </Link>
+                            ))) : (
+                        interviews.map(interview => (
                             <Link to={`/interview/${interview.id}`}>
                                 <Interview
                                     key={interview.id}
                                     vacancy={interview}
                                 />
                             </Link>
-                        ))) : (
-                    interviews.map(interview => (
-                        <Link to={`/interview/${interview.id}`}>
-                            <Interview
-                                key={interview.id}
-                                vacancy={interview}
-                            />
-                        </Link>
-                    )))}
+                        )))}
+                </div>
             </div>
-        </div>
+        </>
+
     );
 }
 
